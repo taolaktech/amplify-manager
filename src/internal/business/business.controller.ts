6093a4603a5db0c4e547b5dde5f469b6d2c6e875
Akinolaaa <akin.akinleye619@gmail.com>
2026-01-16 17:35:23 +0100

    chore: removing deprecated code

--- src/internal/business/business.controller.ts
+++ src/internal/business/business.controller.ts
@@ -1,7 +1,6 @@
-import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { InternalBusinessService } from './business.service';
-import { ApiOperation, ApiResponse, ApiSecurity } from '@nestjs/swagger';
-import { SaveGoogleAdsCustomerDataDto } from './dto/save-googleads-data.dto';
+import { ApiSecurity } from '@nestjs/swagger';
 import { CalcTargetRoasDto } from './dto/calculate-target-roas.dto';
 
 @ApiSecurity('x-api-key')
@@ -15,29 +14,6 @@ export class InternalBusinessController {
   async findOne(@Param('businessId') id: string) {
     const business = await this.internalbusinessService.getBusinessById(id);
     return { business };
-  }
-
-  @Patch('/:businessId/save-googleads-customer-data')
-  @ApiOperation({ summary: 'Save google ads customer data for user' })
-  @ApiResponse({
-    status: 200,
-    description: 'Data saved successfully',
-  })
-  @ApiResponse({
-    status: 409,
-    description: 'Data already exists',
-  })
-  async saveGoogleCustomerData(
-    @Param('businessId') businessId: string,
-    @Body() dto: SaveGoogleAdsCustomerDataDto,
-  ) {
-    const business =
-      await this.internalbusinessService.saveGoogleAdsCustomerData(
-        businessId,
-        dto,
-      );
-
-    return business;
   }
 
   @Post('/:businessId/calculate-target-roas')
